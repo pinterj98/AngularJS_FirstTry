1018adce9c377dbdafbdb7b258328244956f5065
Anonime <hidden@example.com>
2021-08-08 20:37:27 +0200

    nemár

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -50,7 +50,7 @@
             }).then(function(response) {
                 var foundItems = [];
                 for (var i = 0; i < response.data['menu_items'].length; i++) {
-                    if (response.data['menu_items'][i]['description'].toLowerCase().indexOf(searchTerm) !== -1) {
+                    if (searchTerm.length > 0 && response.data['menu_items'][i]['description'].toLowerCase().indexOf(searchTerm) !== -1) {
                         foundItems.push(response.data['menu_items'][i]);
                     }
                 }
