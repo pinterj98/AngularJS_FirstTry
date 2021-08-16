5705f9ef0f93e8dcf7de13face680d6c530e22e4
pinterj98 <71833351+pinterj98@users.noreply.github.com>
2021-08-16 21:25:23 +0200

    Add files via upload

---
+++ module4-solution/categories.components.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+    angular.module('MenuApp')
+        .component('categories', {
+           templateUrl: 'categories.component.html',
+            bindings: {categories: '<'}
+        });
+})();
