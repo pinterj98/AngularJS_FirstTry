82b6e9f0a1889f649c92bfd2ed4738b31a4127d5
Anonime <hidden@example.com>
2021-08-16 22:30:28 +0200

    doneasd

---
+++ module4/components/categories.component.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+    angular.module('MenuApp')
+        .component('categories', {
+           templateUrl: 'templates/categories.component.html',
+            bindings: {categories: '<'}
+        });
+})();
